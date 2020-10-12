30114942edca0641f43908dd66106356b31f2a5e
tangjiadong <tang897396324@163.com>
2020-10-12 23:34:00 +0800

    init project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+//导入全局样式表
+import './assets/css/global.css'
+//导入字体图标
+import './assets/fonts/iconfont.css'
+//引用element-ui组件
+import ElementUI from 'element-ui' //element-ui的全部组件
+import 'element-ui/lib/theme-chalk/index.css'//element-ui的css
+Vue.use(ElementUI) //使用elementUI
 
 Vue.config.productionTip = false
 
