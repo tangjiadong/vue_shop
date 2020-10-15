7170e7f25a63a7802e704ac20fc28e54fb86fa2a
tangjiadong <tang897396324@163.com>
2020-10-15 16:39:50 +0800

    Update Users.vue

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -293,21 +293,25 @@ export default {
     async removeUserById(id) {
       console.log(id)
       //弹框询问用户是否删除数据
-      const confirmResult = await this.$confirm('此操作将永久删除该用户, 是否继续?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning',
-      }).catch(err => err)
+      const confirmResult = await this.$confirm(
+        '此操作将永久删除该用户, 是否继续?',
+        '提示',
+        {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning',
+        }
+      ).catch((err) => err)
       //如果确认删除,返回值为字符串confirm
       //如果取消删除,返回值为字符串cancel
       console.log(confirmResult)
-      if(confirmResult !== 'confirm'){
+      if (confirmResult !== 'confirm') {
         return this.$message.info('已经取消删除')
       }
       // console.log('确认删除')
       //删除请求
-      const {data:res} = await this.$http.delete('users/'+id)
-      if(res.meta.status !== 200){
+      const { data: res } = await this.$http.delete('users/' + id)
+      if (res.meta.status !== 200) {
         return this.$message.error('删除用户失败!')
       }
       this.$message.success('删除用户成功!')
