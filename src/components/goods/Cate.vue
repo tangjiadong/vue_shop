f0f62700d0e23a83b3bf81eedd9297f96f62c305
tangjiadong <tang897396324@163.com>
2020-10-21 17:18:30 +0800

    commit

---
+++ src/components/goods/Cate.vue
@@ -0,0 +1,139 @@
+<template>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <el-row>
+        <el-col>
+          <el-button type="primary" @click="showAddCateDialog">添加分类</el-button>
+        </el-col>
+      </el-row>
+      <!-- 表格 -->
+      <tree-table class="treeTable" :data="catelist" :columns="columns" :selection-type="false" :expand-type="false"
+        show-index index-text="#" border :show-row-hover="false">
+        <!-- 是否有效 -->
+        <template slot="isok" slot-scope="scope">
+          <i class="el-icon-success" v-if="scope.row.cat_deleted === false" style="color: lightgreen;"></i>
+          <i class="el-icon-error" v-else style="color: red;"></i>
+        </template>
+        <!-- 排序 -->
+        <template slot="order" slot-scope="scope">
+          <el-tag size="mini" v-if="scope.row.cat_level===0">一级</el-tag>
+          <el-tag size="mini" type="scuccess" v-else-if="scope.row.cat_level===1">二级</el-tag>
+          <el-tag size="mini" type="warning" v-else>三级</el-tag>
+        </template>
+        <!-- 操作 -->
+        <template slot="opt" slot-scope>
+          <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+          <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+        </template>
+      </tree-table>
+
+      <!-- 分页 -->
+      <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum" :page-sizes="[3,5,10,15]" :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper" :total="total">
+      </el-pagination>
+    </el-card>
+
+    <!-- 添加分类的对话框 -->
+    <el-dialog title="添加分类" :visible.sync="addCateDialogVisible" width="50%">
+      <span>这是一段信息</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addCateDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addCateDialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      queryInfo: {
+        type: 3,
+        pagenum: 1,
+        pagesize: 5,
+      },
+      // 商品分类的数据列表，默认为空
+      catelist: [],
+      // 总数据条数
+      total: 0,
+      // table指定列的定义
+      columns: [
+        {
+          label: '分类名称',
+          prop: 'cat_name',
+        },
+        {
+          label: '是否有效',
+          //表示将当前列定义为模板列
+          type: 'template',
+          //表示当前这一列使用模板名称
+          template: 'isok',
+        },
+        {
+          label: '排序',
+          //表示将当前列定义为模板列
+          type: 'template',
+          //表示当前这一列使用模板名称
+          template: 'order',
+        },
+        {
+          label: '操作',
+          //表示将当前列定义为模板列
+          type: 'template',
+          //表示当前这一列使用模板名称
+          template: 'opt',
+        },
+      ],
+      //控制添加分类对话框的显示与隐藏
+      addCateDialogVisible: false,
+    }
+  },
+  created() {
+    this.getCateList()
+  },
+  methods: {
+    //获取商品分类数据
+    async getCateList() {
+      const { data: res } = await this.$http.get('categories', {
+        params: this.queryInfo,
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取商品分类失败!')
+      }
+      //   console.log(res.data)
+      this.catelist = res.data.result
+      this.total = res.data.total
+    },
+    //监听pagesize改变
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getCateList()
+    },
+    //监听pagenum改变
+    handleCurrentChange(newPage) {
+      this.queryInfo.pagenum = newPage
+      this.getCateList()
+    },
+    //点击按钮,展示添加分类的对话框
+    showAddCateDialog() {
+      this.addCateDialogVisible = true
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.treeTable {
+  margin-top: 15px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import router from './router'
 import './assets/css/global.css'
 //导入字体图标
 import './assets/fonts/iconfont.css'
+//导入vue-tree-with-tree-grid插件
+import TreeTable from 'vue-table-with-tree-grid'
 
 import axios from 'axios'
 //配置请求的根路径
@@ -23,6 +25,8 @@ import 'element-ui/lib/theme-chalk/index.css'//element-ui的css
 Vue.use(ElementUI) //使用elementUI
 
 Vue.config.productionTip = false
+
+Vue.component('tree-table',TreeTable)
 
 new Vue({
   router,
