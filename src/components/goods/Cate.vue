1fad95e6f2f3313a2bdb6aed8f343d2b9cbb6fc5
tangjiadong <tang897396324@163.com>
2020-10-30 09:08:31 +0800

    Update Cate.vue

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -59,7 +59,7 @@
       </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="addCateDialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addCateDialogVisible = false">确 定</el-button>
+        <el-button type="primary" @click="addCate">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -182,6 +182,23 @@ export default {
     //选择项发生变化触发这个函数
     parentCateChanged(){
         console.log(this.selectedKeys)
+        //如果 selectedKeys数组中的length大于0,证明选择的父级分类
+        //反之 就说明没有选择任何的父级分类
+        if(this.selectedKeys.length > 0){
+            //父级分类的id
+            this.addCateForm.cat_pid = this.selectedKeys[this.selectedKeys.length - 1]
+            //为当前分类的等级复制
+            this.addCateForm.cat_level = this.selectedKeys.length
+            return
+        }else {
+            //父级分类id
+            this.addCateForm.cat_pid = 0
+            //为当前分类的等级赋值
+            this.addCateForm.cat_level = 0
+        }
+    },
+    addCate(){
+        console.log(this.addCateForm)
     }
   },
 }
